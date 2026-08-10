31c1598ddbb19dc64915b982d1d99f6a77cc7188
aadamtrunk-maker <aadamtrunk@gmail.com>
2026-08-10 10:54:19 +0530

    Add files via upload

---
+++ src/data/skus.ts
@@ -0,0 +1,614 @@
+import { SKU } from "../types";
+
+export const DEFAULT_SKUS: SKU[] = [
+  {
+    id: 1,
+    srNo: 1,
+    name: "TRYO YELLOW ACID",
+    packing: "1LTR",
+    boxPacking: "12 PC",
+    boxPrice: 220,
+    perPcCost: 18.333,
+    retailPrice: 30
+  },
+  {
+    id: 2,
+    srNo: 2,
+    name: "RED ACID",
+    packing: "1LTR",
+    boxPacking: "12 PC",
+    boxPrice: 270,
+    perPcCost: 22.5,
+    retailPrice: 40
+  },
+  {
+    id: 3,
+    srNo: 3,
+    name: "TRYO WHITE FLOOR CL.",
+    packing: "1LTR",
+    boxPacking: "12 PC",
+    boxPrice: 270,
+    perPcCost: 22.5,
+    retailPrice: 40
+  },
+  {
+    id: 4,
+    srNo: 4,
+    name: "TRYO WHITE FLOOR CL.",
+    packing: "5LTR",
+    boxPacking: "2 PC",
+    boxPrice: 220,
+    perPcCost: 110,
+    retailPrice: 130
+  },
+  {
+    id: 5,
+    srNo: 5,
+    name: "TRYO COLOUR FLOOR",
+    packing: "1LTR",
+    boxPacking: "12 PC",
+    boxPrice: 320,
+    perPcCost: 26.667,
+    retailPrice: 50
+  },
+  {
+    id: 6,
+    srNo: 6,
+    name: "TRYO COLOUR FLOOR",
+    packing: "5LTR",
+    boxPacking: "2 PC",
+    boxPrice: 240,
+    perPcCost: 120,
+    retailPrice: 160
+  },
+  {
+    id: 7,
+    srNo: 7,
+    name: "KLINUP WHITE FLOOR CL.",
+    packing: "1LTR",
+    boxPacking: "12 PC",
+    boxPrice: 250,
+    perPcCost: 20.833,
+    retailPrice: 30
+  },
+  {
+    id: 8,
+    srNo: 8,
+    name: "KLINUP COLOUR FLOOR",
+    packing: "1LTR",
+    boxPacking: "12 PC",
+    boxPrice: 270,
+    perPcCost: 22.5,
+    retailPrice: 40
+  },
+  {
+    id: 9,
+    srNo: 9,
+    name: "KLINUP COLOUR FLOOR",
+    packing: "5LTR",
+    boxPacking: "2 PC",
+    boxPrice: 220,
+    perPcCost: 110,
+    retailPrice: 150
+  },
+  {
+    id: 10,
+    srNo: 10,
+    name: "TRYO BLACK FLOOR CL.",
+    packing: "400ML",
+    boxPacking: "24 PC",
+    boxPrice: 600,
+    perPcCost: 25,
+    retailPrice: 40
+  },
+  {
+    id: 11,
+    srNo: 11,
+    name: "9' O CLOCK BLACK FLOOR CL.",
+    packing: "450ML",
+    boxPacking: "24 PC",
+    boxPrice: 780,
+    perPcCost: 32.5,
+    retailPrice: 50
+  },
+  {
+    id: 12,
+    srNo: 12,
+    name: "9' O CLOCK BLACK FLOOR CL.",
+    packing: "5LTR",
+    boxPacking: "2 PC",
+    boxPrice: 500,
+    perPcCost: 250,
+    retailPrice: 300
+  },
+  {
+    id: 13,
+    srNo: 13,
+    name: "EXTRA STRONG",
+    packing: "400ML",
+    boxPacking: "24 PC",
+    boxPrice: 1080,
+    perPcCost: 45,
+    retailPrice: 60
+  },
+  {
+    id: 14,
+    srNo: 14,
+    name: "EXTRA STRONG",
+    packing: "900ML",
+    boxPacking: "12 PC",
+    boxPrice: 1080,
+    perPcCost: 90,
+    retailPrice: 120
+  },
+  {
+    id: 15,
+    srNo: 15,
+    name: "EXTRA STRONG",
+    packing: "5 LTR",
+    boxPacking: "2 PC",
+    boxPrice: 1000,
+    perPcCost: 500,
+    retailPrice: 500
+  },
+  {
+    id: 16,
+    srNo: 16,
+    name: "SILVER GREEN",
+    packing: "200ML",
+    boxPacking: "24 PC",
+    boxPrice: 660,
+    perPcCost: 27.5,
+    retailPrice: 40
+  },
+  {
+    id: 17,
+    srNo: 17,
+    name: "SILVER GREEN",
+    packing: "500ML",
+    boxPacking: "24 PC",
+    boxPrice: 1680,
+    perPcCost: 70,
+    retailPrice: 100
+  },
+  {
+    id: 18,
+    srNo: 18,
+    name: "GOLDEN CONCENTRATE",
+    packing: "200ML",
+    boxPacking: "24 PC",
+    boxPrice: 960,
+    perPcCost: 40,
+    retailPrice: 50
+  },
+  {
+    id: 19,
+    srNo: 19,
+    name: "TOILET CLEANER",
+    packing: "250ML",
+    boxPacking: "24 PC",
+    boxPrice: 600,
+    perPcCost: 25,
+    retailPrice: 40
+  },
+  {
+    id: 20,
+    srNo: 20,
+    name: "TOILET CLEANER",
+    packing: "500ML",
+    boxPacking: "24 PC",
+    boxPrice: 960,
+    perPcCost: 40,
+    retailPrice: 60
+  },
+  {
+    id: 21,
+    srNo: 21,
+    name: "TOILET CLEANER",
+    packing: "1LTR",
+    boxPacking: "12 PC",
+    boxPrice: 840,
+    perPcCost: 70,
+    retailPrice: 100
+  },
+  {
+    id: 22,
+    srNo: 22,
+    name: "TOILET CLEANER",
+    packing: "5LTR",
+    boxPacking: "2 PC",
+    boxPrice: 500,
+    perPcCost: 250,
+    retailPrice: 350
+  },
+  {
+    id: 23,
+    srNo: 23,
+    name: "RED BATHROOM CLEANER",
+    packing: "500ML",
+    boxPacking: "24 PC",
+    boxPrice: 1020,
+    perPcCost: 42.5,
+    retailPrice: 60
+  },
+  {
+    id: 24,
+    srNo: 24,
+    name: "RED BATHROOM CLEANER",
+    packing: "5LTR",
+    boxPacking: "2 PC",
+    boxPrice: 550,
+    perPcCost: 275,
+    retailPrice: 350
+  },
+  {
+    id: 25,
+    srNo: 25,
+    name: "TILES CLEANER",
+    packing: "650ML",
+    boxPacking: "24 PC",
+    boxPrice: 1320,
+    perPcCost: 55,
+    retailPrice: 80
+  },
+  {
+    id: 26,
+    srNo: 26,
+    name: "GLASS CLEANER",
+    packing: "500ML",
+    boxPacking: "24 PC",
+    boxPrice: 1200,
+    perPcCost: 50,
+    retailPrice: 70
+  },
+  {
+    id: 27,
+    srNo: 27,
+    name: "GLASS CLEANER",
+    packing: "5LTR",
+    boxPacking: "2 PC",
+    boxPrice: 600,
+    perPcCost: 300,
+    retailPrice: 400
+  },
+  {
+    id: 28,
+    srNo: 28,
+    name: "GLOW FLOOR CLEANER",
+    packing: "500ML",
+    boxPacking: "24 PC",
+    boxPrice: 1080,
+    perPcCost: 45,
+    retailPrice: 60
+  },
+  {
+    id: 29,
+    srNo: 29,
+    name: "9 O'CLOCK HAND SOAP MIX FLVR.",
+    packing: "500ML",
+    boxPacking: "12 PC",
+    boxPrice: 780,
+    perPcCost: 65,
+    retailPrice: 80
+  },
+  {
+    id: 30,
+    srNo: 30,
+    name: "9 O'CLOCK HAND SOAP",
+    packing: "1LTR",
+    boxPacking: "12 PC",
+    boxPrice: 1200,
+    perPcCost: 100,
+    retailPrice: 120
+  },
+  {
+    id: 31,
+    srNo: 31,
+    name: "9 O'CLOCK HAND SOAP",
+    packing: "5LTR",
+    boxPacking: "2 PC",
+    boxPrice: 1000,
+    perPcCost: 500,
+    retailPrice: 550
+  },
+  {
+    id: 32,
+    srNo: 32,
+    name: "AFLATOON LIQUID BLEACH",
+    packing: "1LTR",
+    boxPacking: "12 PC",
+    boxPrice: 440,
+    perPcCost: 36.667,
+    retailPrice: 60
+  },
+  {
+    id: 33,
+    srNo: 33,
+    name: "SONU WHITE FLOOR CL.",
+    packing: "4.5 LTR",
+    boxPacking: "2 PC",
+    boxPrice: 180,
+    perPcCost: 90,
+    retailPrice: 100
+  },
+  {
+    id: 34,
+    srNo: 34,
+    name: "SONU LIQUID SOAP",
+    packing: "4.5 LTR",
+    boxPacking: "2 PC",
+    boxPrice: 220,
+    perPcCost: 110,
+    retailPrice: 130
+  },
+  {
+    id: 35,
+    srNo: 35,
+    name: "KLIN-UP LIQUID SOAP",
+    packing: "1LTR",
+    boxPacking: "12 PC",
+    boxPrice: 420,
+    perPcCost: 35,
+    retailPrice: 50
+  },
+  {
+    id: 36,
+    srNo: 36,
+    name: "KLIN-UP LIQUID SOAP",
+    packing: "4.5 LTR",
+    boxPacking: "2 PC",
+    boxPrice: 260,
+    perPcCost: 130,
+    retailPrice: 160
+  },
+  {
+    id: 37,
+    srNo: 37,
+    name: "TRYO ALL IN ONE D W",
+    packing: "1LTR",
+    boxPacking: "12 PC",
+    boxPrice: 480,
+    perPcCost: 40,
+    retailPrice: 60
+  },
+  {
+    id: 38,
+    srNo: 38,
+    name: "TRYO ALL IN ONE D W",
+    packing: "3LTR",
+    boxPacking: "4 PC",
+    boxPrice: 520,
+    perPcCost: 130,
+    retailPrice: 150
+  },
+  {
+    id: 39,
+    srNo: 39,
+    name: "TRYO ALL IN ONE D W",
+    packing: "5LTR",
+    boxPacking: "2 PC",
+    boxPrice: 380,
+    perPcCost: 190,
+    retailPrice: 230
+  },
+  {
+    id: 40,
+    srNo: 40,
+    name: "GLOW DISH WASH",
+    packing: "1 LTR",
+    boxPacking: "12 PC",
+    boxPrice: 1020,
+    perPcCost: 85,
+    retailPrice: 120
+  },
+  {
+    id: 41,
+    srNo: 41,
+    name: "GLOW DISH WASH",
+    packing: "5 LTR",
+    boxPacking: "2 PC",
+    boxPrice: 700,
+    perPcCost: 350,
+    retailPrice: 380
+  },
+  {
+    id: 42,
+    srNo: 42,
+    name: "GLOW LIQUID DETER.",
+    packing: "5 LTR",
+    boxPacking: "2 PC",
+    boxPrice: 660,
+    perPcCost: 330,
+    retailPrice: 400
+  },
+  {
+    id: 43,
+    srNo: 43,
+    name: "CHAMKILA LIQUID DETER.",
+    packing: "5LTR",
+    boxPacking: "2 PC",
+    boxPrice: 900,
+    perPcCost: 450,
+    retailPrice: 550
+  },
+  {
+    id: 44,
+    srNo: 44,
+    name: "TRYO LIQUID DETER.",
+    packing: "5LTR",
+    boxPacking: "2 PC",
+    boxPrice: 950,
+    perPcCost: 475,
+    retailPrice: 650
+  },
+  {
+    id: 45,
+    srNo: 45,
+    name: "OXI ACTIVE DETERGENT",
+    packing: "5LTR",
+    boxPacking: "2 PC",
+    boxPrice: 1300,
+    perPcCost: 650,
+    retailPrice: 750
+  },
+  {
+    id: 46,
+    srNo: 46,
+    name: "OXI ACTIVE DETERGENT",
+    packing: "1LTR",
+    boxPacking: "6 PC",
+    boxPrice: 780,
+    perPcCost: 130,
+    retailPrice: 160
+  },
+  {
+    id: 47,
+    srNo: 47,
+    name: "AFLATOON AFTER WASH MIX FLVR.",
+    packing: "500ML",
+    boxPacking: "12 PC",
+    boxPrice: 840,
+    perPcCost: 70,
+    retailPrice: 100
+  },
+  {
+    id: 48,
+    srNo: 48,
+    name: "AFLATOON AFTER WASH MIX FLVR.",
+    packing: "1LTR",
+    boxPacking: "12 PC",
+    boxPrice: 1560,
+    perPcCost: 130,
+    retailPrice: 150
+  },
+  {
+    id: 49,
+    srNo: 49,
+    name: "CHAMKILA POWDER",
+    packing: "200GMS",
+    boxPacking: "24 PC",
+    boxPrice: 300,
+    perPcCost: 12.5,
+    retailPrice: 20
+  },
+  {
+    id: 50,
+    srNo: 50,
+    name: "DRAIN CLEANER",
+    packing: "20GMS",
+    boxPacking: "12 PC",
+    boxPrice: 150,
+    perPcCost: 12.5,
+    retailPrice: 20
+  },
+  {
+    id: 51,
+    srNo: 51,
+    name: "METAL CLEANER",
+    packing: "200ML",
+    boxPacking: "24 PC",
+    boxPrice: 1920,
+    perPcCost: 80,
+    retailPrice: 100
+  },
+  {
+    id: 52,
+    srNo: 52,
+    name: "A+ YELLOW ACID",
+    packing: "1LTR",
+    boxPacking: "12 PC",
+    boxPrice: 190,
+    perPcCost: 15.833,
+    retailPrice: 20
+  },
+  {
+    id: 53,
+    srNo: 53,
+    name: "A+ COLOUR FLOOR CLEANER",
+    packing: "1LTR",
+    boxPacking: "12 PC",
+    boxPrice: 280,
+    perPcCost: 23.333,
+    retailPrice: 40
+  },
+  {
+    id: 54,
+    srNo: 54,
+    name: "A+ COLOUR FLOOR CLEANER",
+    packing: "5LTR",
+    boxPacking: "4 PC",
+    boxPrice: 400,
+    perPcCost: 100,
+    retailPrice: 140
+  },
+  {
+    id: 55,
+    srNo: 55,
+    name: "A+ BLEACH",
+    packing: "1LTR",
+    boxPacking: "12 PC",
+    boxPrice: 330,
+    perPcCost: 27.5,
+    retailPrice: 50
+  },
+  {
+    id: 56,
+    srNo: 56,
+    name: "A+ EXTRA STRONG",
+    packing: "400ML",
+    boxPacking: "30 PC",
+    boxPrice: 1125,
+    perPcCost: 37.5,
+    retailPrice: 50
+  },
+  {
+    id: 57,
+    srNo: 57,
+    name: "A+ EXTRA STRONG",
+    packing: "1LTR",
+    boxPacking: "15 PC",
+    boxPrice: 1125,
+    perPcCost: 75,
+    retailPrice: 100
+  },
+  {
+    id: 58,
+    srNo: 58,
+    name: "A+ EXTRA STRONG",
+    packing: "5LTR",
+    boxPacking: "2 PC",
+    boxPrice: 700,
+    perPcCost: 350,
+    retailPrice: 400
+  },
+  {
+    id: 59,
+    srNo: 59,
+    name: "A+ DISHWASH",
+    packing: "1LTR",
+    boxPacking: "15 PC",
+    boxPrice: 450,
+    perPcCost: 30,
+    retailPrice: 50
+  },
+  {
+    id: 60,
+    srNo: 60,
+    name: "A+ MOSQUITOES SPRAY",
+    packing: "1LTR",
+    boxPacking: "15 PC",
+    boxPrice: 900,
+    perPcCost: 60,
+    retailPrice: 100
+  },
+  {
+    id: 61,
+    srNo: 61,
+    name: "A+ AND WASH",
+    packing: "500ML",
+    boxPacking: "24 PC",
+    boxPrice: 1200,
+    perPcCost: 50,
+    retailPrice: 80
+  }
+];
